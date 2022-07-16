ea93d30b2448ab3c3b15bc1495872f46ef7361b9
carlastevenson <33378078+carlastevenson@users.noreply.github.com>
2022-07-16 19:15:20 -0400

    Create PrintingWithExponents.py

---
+++ PrintingWithExponents.py
@@ -0,0 +1,13 @@
+import random
+clist=[]
+explst=[]
+s=""
+for i in range(10):
+    clist.append(randrange(10,10,1)
+    explst.append(i)
+def print_equation2():
+    for i in range(len(clist)):
+        term="+(%s)"%clist[i]+"x"+explst[i]
+        s+=term
+    print(s)
+print_equation2()                 
